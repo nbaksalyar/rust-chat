49b4c2afa8a13db6fa351c0d15abd2414d1216f4
Nikita Baksalyar <nikita.baksalyar@gmail.com>
2016-02-07 16:05:13 +0300

    Fix mio 0.5 compatibility

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,39 @@
 
 extern crate mio_websocket;
+extern crate env_logger;
 
 use std::net::SocketAddr;
 
 use mio_websocket::interface::*;
 
 fn main() {
-    let (events, mut ws) = WebSocket::new("0.0.0.0:10000".parse::<SocketAddr>().unwrap());
+    env_logger::init().unwrap();
 
-    for event in events {
-        match event {
+    let mut ws = WebSocket::new("0.0.0.0:10000".parse::<SocketAddr>().unwrap());
+
+    loop {
+        match ws.next() {
             WebSocketEvent::Connect(tok) => {
                 println!("connected peer: {:?}", tok);
-
-                ws.send(WebSocketEvent::TextMessage(tok, "Hello!".to_string()));
             },
 
             WebSocketEvent::TextMessage(tok, msg) => {
                 println!("msg from {:?}", tok);
 
-                for peer in ws.get_peers() {
-                    println!("-> relaying to peer {:?}", peer);
+                for peer in ws.get_connected().unwrap() {
+                    if peer != tok {
+                        println!("-> relaying to peer {:?}", peer);
 
-                    let response = WebSocketEvent::TextMessage(peer, format!("{:?} says \"{}\"", tok, msg));
-                    ws.send(response);
+                        let response = WebSocketEvent::TextMessage(peer, msg.clone());
+                        ws.send(response);
+                    }
                 }
+            },
+
+            WebSocketEvent::BinaryMessage(tok, msg) => {
+                println!("msg from {:?}", tok);
+                let response = WebSocketEvent::BinaryMessage(tok, msg.clone());
+                ws.send(response);
             },
 
             _ => {}
